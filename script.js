0c3cfedf8fabf28ef5a7766d695a8d08976d7dc6
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-18 17:42:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const makeChange = (c) => {
   // your name here
+	const values=[25,10,5,1];
+	const v=[0, 0,  0,  0 ]
+	let i=0
+	while(c>0){
+		if(c>=values[i]){
+			c=c-values[i]
+			v[i]+=1
+		}else{
+			i++
+		}
+	}
+	return {"q":v[0],"d":v[1],"n":v[2],"p":v[3]}
+	
 };
 
 // Do not the change the code below
